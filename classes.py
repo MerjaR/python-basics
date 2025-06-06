605d071b92df4eeefd9b5de7206abc2abcf9d837
MerjaR <merja.rossi@gmail.com>
2025-06-07 01:40:43 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -5,6 +5,25 @@ class Book:
         self.name = name
         self.year = year
         self.genre = genre
+        
+class Checklist:
+    def __init__(self, header: str, entries: list):
+        self.header = header
+        self.entries = entries
+    
+class Customer:
+    def __init__(self, id: str, balance: float, discount: int):
+        self.id = id
+        self.balance = balance
+        self.discount = discount
+
+class Cable:
+    def __init__(self, model: str, length: float, max_speed: int, bidirectional: bool):
+        self.model = model
+        self.length = length
+        self.max_speed = max_speed
+        self.bidirectional = bidirectional
+
 
 if __name__ == "__main__":
     python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
@@ -12,3 +31,4 @@ if __name__ == "__main__":
 
     print(f"{python.author}: {python.name} ({python.year})")
     print(f"The genre of the book {everest.name} is {everest.genre}")
+
