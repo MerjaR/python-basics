430cabd15529f1768943c8ae2e9737af63cca7e8
MerjaR <merja.rossi@gmail.com>
2025-08-04 17:41:00 +0300

    Create add_numbers_to_list.py

---
+++ add_numbers_to_list.py
@@ -0,0 +1,12 @@
+# Recursion
+def add_numbers_to_list(numbers: list):
+    while len(numbers)%5 != 0:
+        current = numbers[len(numbers) -1]
+        number = current + 1
+        numbers.append(number)
+        add_numbers_to_list(numbers)
+
+if __name__ == "__main__":
+    numbers = [1,3,4,5,10,11]
+    add_numbers_to_list(numbers)
+    print(numbers)
